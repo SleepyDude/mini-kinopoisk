00df68d2d27f2cf5fabd44aa674feb8518a34886
Igor <char.habit@gmail.com>
2023-04-19 17:34:35 +0400

    create tables for movies

--- server/movies/src/app.module.ts
+++ server/movies/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { FilmsModule } from './films/films.module';
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeModule} from "@nestjs/sequelize";
+import { GenresModule } from './genres/genres.module';
+import { CountriesModule } from './countries/countries.module';
+import { BudgetModule } from './budget/budget.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT_OUTSIDE),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB_MOVIES,
+      models: [],
+      autoLoadModels: true,
+    }),
+      FilmsModule,
+      GenresModule,
+      CountriesModule,
+      BudgetModule,
+      ReviewsModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ server/movies/src/countries/countries.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CountriesController } from './countries.controller';
+import { CountriesService } from './countries.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {Countries} from "./countries.model";
+import {CountriesFilms} from "./countries.m2m.model";
+
+@Module({
+  controllers: [CountriesController],
+  providers: [CountriesService],
+  imports: [SequelizeModule.forFeature([Countries, CountriesFilms])]
+})
+export class CountriesModule {}
