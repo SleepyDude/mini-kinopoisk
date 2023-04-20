e206d8fc28bf0b1b6d58fdb671994434074cf7cd
alex-ivankova766 <ivankova.alexandra766@gmail.com>
2023-04-20 19:43:16 +0500

    вынужденный коммит, для пула

--- server/auth/src/auth/auth.module.ts
+++ server/auth/src/auth/auth.module.ts
@@ -5,21 +5,6 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'AUTH_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://rabbitmq:5672'],
-          queue: 'users_queue',
-          queueOptions: {
-            durable: false,
-          },
-        },
-      },
-    ]),
-  ],
+  providers: [AuthService]
 })
 export class AuthModule {}
--- server/auth/src/auth/auth.service.ts
+++ server/auth/src/auth/auth.service.ts
@@ -1,16 +1,49 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create.user.dto';
 import { ClientProxy } from '@nestjs/microservices';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcryptjs'
+import { switchMap } from 'rxjs';
 
 @Injectable()
 export class AuthService {
-  constructor(@Inject('AUTH_SERVICE') private readonly client: ClientProxy) {}
+  constructor(@Inject('AUTH_SERVICE') private readonly client: ClientProxy,
+  @Inject('USER_SERVICE') private readonly userService: ClientProxy,) {}
 
-  async login(userDto: CreateUserDto) {
+  private async hashPassword(password: string) {
+    return await bcrypt.hash(password, process.env.SALT);
+  }
+
+  async login(userDto: LoginDto) {
     return 'login';
   }
 
-  async registration(userDto: CreateUserDto) {
+  async registration(userDto: LoginDto) {
+
+    const hashedPassword = this.hashPassword(userDto.password);
+
+    const user$ = this.userService.send( {cmd: 'get-user-by-email' }, userDto.email ).pipe(
+      switchMap((value) => {
+        
+      })
+    )
+
+    // const id$ = this.authService.send({ cmd: 'register' }, registerProfileDto).pipe(
+    //   switchMap((value) => {
+    //       // console.log(`[profiles][registration] service. return from authService: ${JSON.stringify(value)}`);
+    //       const { id } = value;
+    //       // console.log(`id = ${id}`);
+    //       return of(id)
+    //   }),
+    //   catchError( (error) => {
+    //       console.log(`[profiles][registration] catchError error = ${JSON.stringify(error)}`)
+    //       throw new UnauthorizedException(error);
+    //   })
+
+    // const candidate = await this.userService.getUserByEmail(profileDTO.email);
+    // if (candidate) {
+    //     throw new HttpException(`A user with email ${profileDTO.email} already exists`, HttpStatus.BAD_REQUEST)
+    // }
+
     console.log('SERVICE AUTH: ', userDto)
     try {
       let user = await this.client.send({ cmd: 'create_user' }, userDto).toPromise();
