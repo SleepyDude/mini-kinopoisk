cbec0fbc9abcae6a89e55465bd53b5793c8e6e3e
Igor <char.habit@gmail.com>
2023-04-22 13:39:21 +0400

    Припрятал, недоработка

--- server/movies/src/films/films.controller.ts
+++ server/movies/src/films/films.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Param} from '@nestjs/common';
+import {FilmsService} from "./films.service";
 
 @Controller('films')
-export class FilmsController {}
+export class FilmsController {
+
+    constructor(
+        private filmsService: FilmsService,
+    ) {}
+
+    @Get()
+    getAllFilms() {
+        return this.filmsService.getAllFilms();
+    }
+
+    @Get('/:id')
+    getFilmById(@Param('id') id) {
+        return this.filmsService.getFilmById(id);
+    }
+}
--- server/movies/src/films/films.service.ts
+++ server/movies/src/films/films.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import {InjectModel} from "@nestjs/sequelize";
+import {Films} from "./films.model";
 
 @Injectable()
-export class FilmsService {}
+export class FilmsService {
+
+    constructor(
+        @InjectModel(Films) private filmsRepository: typeof Films,
+    ) {}
+    async getAllFilms() {
+        return await this.filmsRepository.findAll();
+    }
+
+    async getFilmById(id) {
+        let result = [];
+        let currentCountries = [];
+        let currentGenres = [];
+        `Должен вернуть:
+        фильм
+        страны
+        жанры
+        актеров`
+
+        const currentFilm = await this.filmsRepository.findOne({ where: { kinopoiskId: id } });
+
+        const currentCountriesId =
+        for ( let countryId of currentCountriesId ) {
+            currentCountries.push(await this.countriesRepository.findOne({
+                where : { id: countryId.countryId };
+            }));
+        }
+
+        const currentGenresId = await this.genresFilmsRepository.findAll({
+            where: { kinopoiskFilmId: currentFilm.kinopoiskId }
+        });
+        for ( let genreId of currentGenresId ) {
+            currentGenres.push(await this.genresRepository.findOne({
+                where : { id: genreId.genreId }
+            }));
+        }
+
+        const
+    }
+}
