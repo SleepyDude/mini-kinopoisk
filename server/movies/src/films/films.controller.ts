653336ff9bf931b5cf5c345aba78a89d9b882901
Igor <char.habit@gmail.com>
2023-04-27 19:02:14 +0400

    изменена функция получения фильмов. Написан фильтр по фильмам. Получение фильмов превью.

--- server/movies/src/films/films.controller.ts
+++ server/movies/src/films/films.controller.ts
@@ -7,17 +7,22 @@ export class FilmsController {
   constructor(private filmsService: FilmsService) {}
 
   @MessagePattern({ cmd: 'get-all-films' })
-  getAllFilms(@Payload() params)  {
-    return this.filmsService.getAllFilms(params);
+  async getAllFilms(@Payload() params) {
+    return await this.filmsService.getAllFilms(params);
   }
 
   @MessagePattern({ cmd: 'get-film-byId' })
-  getFilmById(@Payload() id) {
-    return this.filmsService.getFilmById(id);
+  async getFilmById(@Payload() id) {
+    return await this.filmsService.getFilmById(id);
   }
 
-  @MessagePattern({ cmd: 'test-movies' })
-  test() {
-    return 'Test done';
+  @MessagePattern({ cmd: 'get-films-byId-previous' })
+  async getFilmsByIdPrevious(@Payload() filmsId) {
+    return await this.filmsService.getFilmsByIdPrevious(filmsId);
+  }
+
+  @MessagePattern({ cmd: 'get-films-byFilters' })
+  getFilmsByFilters(@Payload() params) {
+    return this.filmsService.getFilmsByFilers(params);
   }
 }
--- server/movies/src/films/films.service.ts
+++ server/movies/src/films/films.service.ts
@@ -1,25 +1,46 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Films } from './films.model';
-import { CountriesService } from '../countries/countries.service';
-import { GenresService } from '../genres/genres.service';
-import { BudgetService } from '../budget/budget.service';
 import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
 import { Op } from 'sequelize';
+import { Genres } from '../genres/genres.model';
+import { Countries } from '../countries/countries.model';
 
 @Injectable()
 export class FilmsService {
   constructor(
-    @Inject('MOVIES-SERVICE') private usersClient: ClientProxy,
+    @Inject('MOVIES-SERVICE') private moviesClient: ClientProxy,
     @InjectModel(Films) private filmsRepository: typeof Films,
   ) {}
   async getAllFilms(params) {
-    const { page, size, field, value } = params;
-    const condition = value ? { [field]: { [Op.iLike]: `%${value}%` } } : null;
-      const { limit, offset } = this.getPagination(page, size);
+    const { page, size, name } = params;
+    const condition = name ? { nameRu: { [Op.iLike]: `%${name}%` } } : null;
+    const { limit, offset } = this.getPagination(page, size);
 
     return await this.filmsRepository.findAndCountAll({
+      attributes: [
+        'id',
+        'nameRu',
+        'nameOriginal',
+        'posterUrl',
+        'posterUrlPreview',
+        'coverUrl',
+        'logoUrl',
+        'ratingKinopoisk',
+        'year',
+        'filmLength',
+      ],
+      include: [
+        {
+          model: Genres,
+          attributes: ['id', 'genreNameRu', 'genreNameEng'],
+        },
+        {
+          model: Countries,
+          attributes: ['id', 'countryNameRu', 'countryNameEng'],
+        },
+      ],
       where: condition,
       limit,
       offset,
@@ -27,14 +48,113 @@ export class FilmsService {
   }
 
   async getFilmById(id) {
-    return await this.filmsRepository.findOne({
+    const currentFilm = await this.filmsRepository.findOne({
+      attributes: {
+        exclude: [
+          'reviewsCount',
+          'ratingGoodReview',
+          'ratingGoodReviewVoteCount',
+          'ratingFilmCritics',
+          'ratingFilmCriticsVoteCount',
+          'serial',
+          'shortFilm',
+          'createdAt',
+          'updatedAt',
+        ],
+      },
       include: { all: true },
       where: { id: id.id },
     });
+    const currentStaff = await lastValueFrom(
+      this.moviesClient.send({ cmd: 'get-staff-previous' }, currentFilm.id),
+    );
+    return {
+      currentFilm,
+      currentStaff,
+    };
+  }
+
+  async getFilmsByFilers(params) {
+    const films = [];
+    const genres = [];
+    const countries = [];
+
+    const { page, size } = params;
+    const { limit, offset } = this.getPagination(page, size);
+
+    for (const [key, value] of Object.entries(params)) {
+      if (key === 'year' || key === 'type') {
+        films.push({ [key]: value });
+      }
+      if (key === 'ratingKinopoisk' || key === 'ratingKinopoiskVoteCount') {
+        films.push({ [key]: { [Op.gte]: value } });
+      }
+
+      if (key === 'genreId') {
+        genres.push(value);
+      }
+
+      if (key === 'countryId') {
+        countries.push(value);
+      }
+    }
+
+    return this.filmsRepository.findAndCountAll({
+      attributes: [
+        'id',
+        'nameRu',
+        'nameOriginal',
+        'ratingKinopoiskVoteCount',
+        'posterUrl',
+        'posterUrlPreview',
+        'coverUrl',
+        'logoUrl',
+        'ratingKinopoisk',
+        'year',
+        'filmLength',
+        'type',
+      ],
+      where: films,
+      include: [
+        {
+          model: Genres,
+          where: { id: { [Op.or]: genres } },
+        },
+        {
+          model: Countries,
+          where: { id: { [Op.or]: countries } },
+        },
+      ],
+      limit,
+      offset,
+    });
+  }
+
+  async getFilmsByIdPrevious(filmsId) {
+    const films = [];
+    for (const item of filmsId) {
+      films.push(
+        await this.filmsRepository.findAll({
+          where: item.id,
+          attributes: [
+            'id',
+            'year',
+            'nameRu',
+            'nameOriginal',
+            'posterUrl',
+            'posterUrlPreview',
+            'coverUrl',
+            'logoUrl',
+            'ratingKinopoisk',
+          ],
+        }),
+      );
+    }
+    return films;
   }
 
   private getPagination(page, size) {
-    const limit = size ? +size : 3;
+    const limit = size ? +size : 10;
     const offset = page ? page * limit : 0;
 
     return { limit, offset };
