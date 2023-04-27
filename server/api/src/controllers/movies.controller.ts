fe5129075c4610fe22dfc8dc97a24c3f25c92985
Igor <char.habit@gmail.com>
2023-04-27 19:02:47 +0400

    Написан контроллер к фильмам

--- server/api/src/controllers/movies.controller.ts
+++ server/api/src/controllers/movies.controller.ts
@@ -1,29 +1,57 @@
 import {Controller, Get, Inject, Param, Query} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @ApiTags('Фильмы')
 @Controller('movies')
 export class MoviesController {
 
     constructor(
-        @Inject('MOVIES-SERVICE') private usersService: ClientProxy,
+        @Inject('MOVIES-SERVICE') private moviesService: ClientProxy,
+        @Inject('PERSONS-SERVICE') private personsService: ClientProxy,
     ) {}
 
-    // Пагинация и получение всех фильмов
+    @ApiOperation({ summary: 'Каталог фильмов' })
+    @ApiResponse({ status: 200, description: 'Список фильмов с пагинацией для каталога' })
     @Get()
     async getAllFilms(@Query() param) {
-        return this.usersService.send({ cmd : 'get-all-films' }, param);
+        return this.moviesService.send({ cmd: 'get-all-films' }, param);
     }
 
-    //Получение фильма по id
-    @Get('/:id')
+    @ApiOperation({ summary: 'Все о фильме по айди' })
+    @ApiResponse({ status: 200, description: 'Вся информация о фильме' })
+    @Get('/about/:id')
     async getFilmById(@Param() id: number) {
-        return this.usersService.send({ cmd : 'get-film-byId' }, id);
+        return this.moviesService.send({ cmd: 'get-film-byId' }, id);
     }
 
-    //Фильтр - жанры, страны, рейтинг кинопоиска, количество оценок,
-    // Поиск по режиссеру, поиск по актеру
+    @ApiOperation({ summary: 'Фильтр по фильмам' })
+    @ApiResponse({ status: 200, description: 'Фильтрация по квери строке' })
+    @Get('/filters')
+    async getFilmsByFilters(@Query() params) {
+        return this.moviesService.send({ cmd: 'get-films-byFilters' }, params);
+    }
+
+    @ApiOperation({ summary: 'Получение списка жанров' })
+    @ApiResponse( { status: 200, description: 'Выводит список всех жанров' })
+    @Get('/genres')
+    async getAllGenres() {
+        return this.moviesService.send({ cmd: 'get-all-genres' }, {})
+    }
+
+    @ApiOperation({ summary: 'Получение списка стран' })
+    @ApiResponse({ status: 200, description: 'Выводит список всех стран' })
+    @Get('/countries')
+    getAllCountries() {
+        return this.moviesService.send({ cmd: 'get-all-countries' }, {})
+    }
+
+    @ApiOperation({ summary: 'Полный список людей из фильма' })
+    @ApiResponse({ status: 200, description: 'Выводит полный список актеров по филь айди' })
+    @Get('/about/:id/staff')
+    getStaffByFilmId(@Param('id') id) {
+        return this.personsService.send({ cmd: 'get-staff-by-filmId' }, id)
+    }
 
     //Сортировать по количеству оценок на кинопоиске, рейтинг, дата выхода(сперва свежие)
     // и алфавит
