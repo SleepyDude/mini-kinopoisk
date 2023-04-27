dd038c08c3646a9ae110e613cca9baec8547fdc6
Igor <char.habit@gmail.com>
2023-04-27 19:02:58 +0400

    Написан контроллер к персонам

--- server/persons/src/persons/persons.controller.ts
+++ server/persons/src/persons/persons.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { PersonsService } from './persons.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
@@ -6,19 +6,23 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class PersonsController {
   constructor(private personsService: PersonsService) {}
 
-  @MessagePattern({ cmd: 'get-persons' })
-  @Get()
-  get() {
-    return 'hello';
+  @MessagePattern({ cmd: 'get-all-persons' })
+  async getAllPersons(@Payload() params) {
+    return await this.personsService.getAllPersons(params);
   }
 
-  @MessagePattern({ cmd: 'get-staff' })
-  async getStaffByFilmId(@Payload() id) {
-    return await this.personsService.getStaffByFilmId(id);
+  @MessagePattern({ cmd: 'get-staff-previous' })
+  async getStaffByFilmIdPrevious(@Payload() id) {
+    return await this.personsService.getStaffByFilmIdPrevious(id);
   }
 
-  @MessagePattern({ cmd: 'get-person-by-id' })
+  @MessagePattern({ cmd: 'get-person-byId' })
   async getPersonById(@Payload() id) {
     return await this.personsService.getPersonById(id);
+  }
+
+  @MessagePattern({ cmd: 'get-staff-by-filmId' })
+  async getStaffByFilmId(@Payload() id) {
+    return await this.personsService.getStaffByFilmId(id);
   }
 }
