cf444de9730c01b7606c2b3042f69fc49008649e
Igor <char.habit@gmail.com>
2023-04-29 13:30:49 +0400

    fix linter

--- server/movies/src/genres/genres.controller.ts
+++ server/movies/src/genres/genres.controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { GenresService } from './genres.service';
-import { MessagePattern, Payload } from "@nestjs/microservices";
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('genres')
 export class GenresController {
