b655665b29b8d8dabd806fea547ce4684705a2d6
Igor <char.habit@gmail.com>
2023-04-29 14:12:42 +0400

    Обновлен шаред модуль

---
+++ shared/src/dto/movies/update-country.dto.ts
@@ -0,0 +1,4 @@
+export class UpdateCountryDto {
+  countryNameRu?: string;
+  countryNameEng?: string;
+}
---
+++ shared/src/dto/movies/update-genre.dto.ts
@@ -0,0 +1,4 @@
+export class UpdateGenreDto {
+  genreNameRu?: string;
+  genreNameEng?: string;
+}
